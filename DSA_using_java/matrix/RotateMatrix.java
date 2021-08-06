26911a88f64b0ded3601f7688aacf3d982fe1b0f
slayer321 <sachin.maurya7666@gmail.com>
2021-08-06 12:17:33 +0530

    renamed the file

---
+++ DSA_using_java/matrix/RotateMatrix.java
@@ -0,0 +1,7 @@
+package DSA_using_java.matrix;
+
+class RotateMatrix {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ DSA_using_java/matrix/test.java
@@ -0,0 +1,7 @@
+package DSA_using_java.matrix;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
