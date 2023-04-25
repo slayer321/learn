44df62cdd9353f5c4658328a3a2f15600aa04501
slayer321 <sachin.maurya7666@gmail.com>
2023-04-25 18:21:59 +0530

    story 7

--- one2N-bootcamp/Basic_number_filtering_exercise/story7/main.go
+++ one2N-bootcamp/Basic_number_filtering_exercise/story7/main.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
+var Result []int
+
 func oddNumber(numberList []int) []int {
 
 	outList := make([]int, 0)
-	for val := range numberList {
+	for _, val := range numberList {
 		if val%2 != 0 {
 			outList = append(outList, val)
 		}
@@ -22,10 +26,96 @@ func EvenNumber(numberList []int) []int {
 	return outVal
 }
 
-func dynamicFunction(inputString []string) {
+func GreatorThan5(numberList []int) []int {
 
+	outVal := make([]int, 0)
+	for _, val := range numberList {
+		if val > 5 {
+			outVal = append(outVal, val)
+		}
+	}
+	return outVal
 }
 
-func main() {
+func MultipleOf3(numberList []int) []int {
 
+	outVal := make([]int, 0)
+	for _, val := range numberList {
+		if val%3 == 0 {
+			outVal = append(outVal, val)
+		}
+	}
+	return outVal
+}
+
+func primeNumber(numberList []int) []int {
+	outList := make([]int, 0)
+	var flag int
+
+	// Here if the flag is zero then the number is prime
+	for _, val := range numberList {
+		flag = 0
+		for i := 2; i <= val/2; i++ {
+			if val%i == 0 {
+				flag = 1
+				break
+			}
+
+		}
+		if flag == 0 && val > 1 {
+			outList = append(outList, val)
+		}
+
+	}
+	return outList
+}
+
+func dynamicFunction(inputString []string) {
+
+	availableString := []string{"odd", "even", "gt5", "mul3", "prime"}
+	for _, val := range inputString {
+		if Contains(val, availableString) {
+			// call the function
+			switch val {
+			case "odd":
+				Result = oddNumber(Result)
+			case "even":
+				Result = EvenNumber(Result)
+			case "gt5":
+				Result = GreatorThan5(Result)
+			case "mul3":
+				Result = MultipleOf3(Result)
+			case "prime":
+				Result = primeNumber(Result)
+			default:
+				fmt.Println("No function found")
+			}
+		}
+
+	}
+	return
+}
+
+func Contains(s string, arr []string) bool {
+	for _, val := range arr {
+		if val == s {
+			return true
+		}
+	}
+	return false
+}
+
+func removeIndex(index int, slice []int) []int {
+	return append(slice[:index], slice[index+1:]...)
+}
+
+// odd, greater than 5, multiple of 3
+func main() {
+	// val := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// gg := removeIndex(3, val)
+	// fmt.Println(gg)
+	Result = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	inputString := []string{"odd", "gt5", "mul3", "prime"}
+	dynamicFunction(inputString)
+	fmt.Println(Result)
 }
--- one2N-bootcamp/Generics/main.go
+++ one2N-bootcamp/Generics/main.go
@@ -11,11 +11,64 @@ func reverse[T any](s []T) []T {
 	return r
 }
 
-func main() {
-	out := "sachin"
-
-	for _, val := range out {
-		//fmt.Println(val)
-		fmt.Printf("%x\n", val)
+func SumIntsOrFloats[K comparable, V float64 | int64](m map[K]V) V {
+	var sum V
+	for _, v := range m {
+		sum += v
 	}
+	return sum
+
+}
+
+type Stack[T any] []T
+
+func (s *Stack[T]) Push(v T) {
+	*s = append(*s, v)
+}
+
+func (s *Stack[T]) Pop() T {
+	if len(*s) == 0 {
+		fmt.Println("Stack is empty")
+	}
+
+	x := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return x
+}
+
+func main() {
+	// out := "sachin"
+
+	// for _, val := range out {
+	// 	//fmt.Println(val)
+	// 	fmt.Printf("%x\n", val)
+	// }
+	// Initialize a map for the integer values
+	// ints := map[string]int64{
+	// 	"first":  34,
+	// 	"second": 12,
+	// }
+
+	// // Initialize a map for the float values
+	// floats := map[string]float64{
+	// 	"first":  35.98,
+	// 	"second": 26.99,
+	// }
+
+	// fmt.Printf("Generics %v, %v\n", SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
+
+	s := make(Stack[int], 0)
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+
+	k := make(Stack[string], 0)
+	k.Push("sachin")
+	k.Push("kumar")
+	fmt.Println(k.Pop())
+	fmt.Println(k.Pop())
 }
