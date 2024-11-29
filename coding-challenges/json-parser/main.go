8699bb01a1e2f44c22d6b2bf58a44ab800cae68b
slayer321 <sachin.maurya7666@gmail.com>
2024-11-29 20:18:51 +0530

    adds step 1 for json parser

--- coding-challenges/json-parser/main.go
+++ coding-challenges/json-parser/main.go
@@ -11,7 +11,7 @@ var (
 	dir = "./tests/step"
 )
 
-func lexer(fileName string) {
+func lexer(fileName string) []string {
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("Unable to read file in lexer %v\n", err)
@@ -19,15 +19,35 @@ func lexer(fileName string) {
 	var s scanner.Scanner
 	s.Init(f)
 
+	var sliceToken []string
 	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
-		fmt.Printf("output %v\n", s.TokenText())
+		// fmt.Printf("output %v\n", s.TokenText())
+		sliceToken = append(sliceToken, s.TokenText())
 	}
+
+	return sliceToken
 }
 
-func Parser(fileName string) {
+func parser(tokeSlice []string) int {
 
-	lexer(fileName)
+	if len(tokeSlice) == 0 {
+		fmt.Println("Invalid Json")
+		return 1
+	}
 
+	if tokeSlice[0] == "{" && tokeSlice[1] == "}" {
+		fmt.Println("Valid Json")
+		return 0
+	}
+	return -1
+}
+
+func jsonParser(fileName string) int {
+
+	tokenSlice := lexer(fileName)
+
+	exitCode := parser(tokenSlice)
+	return exitCode
 }
 
 func main() {
@@ -40,17 +60,24 @@ func main() {
 	// validJson := "./test/step1/valid.json"
 	// inValidJson := "./test/step1/invalid.json"
 
-	folderNumber := os.Args[1:]
-
-	files, err := os.ReadDir(dir + folderNumber[0])
-	if err != nil {
-		log.Fatalf("Unable to read files %v\n", err)
+	filename := os.Args[1:]
+	exitCode := jsonParser(filename[0])
+	if exitCode != 0 {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
 	}
+	// folderNumber := os.Args[1:]
 
-	for _, file := range files {
-		fmt.Printf("Printing the file name %s\n", file.Name())
-		Parser(fmt.Sprintf("%s%s/%s", dir, folderNumber[0], file.Name()))
-	}
+	// files, err := os.ReadDir(dir + folderNumber[0])
+	// if err != nil {
+	// 	log.Fatalf("Unable to read files %v\n", err)
+	// }
+	// for _, file := range files {
+	// 	// fmt.Printf("Printing the file name %s\n", file.Name())
+	// 	// jsonParser(fmt.Sprintf("%s%s/%s", dir, folderNumber[0], file.Name()))
+	// 	jsonParser(fmt.Sprintf("%s%s/%s", dir, folderNumber[0], file.Name()))
+	// }
 
 	// 	var s scanner.Scanner
 	// 	s.Init(strings.NewReader(src))
